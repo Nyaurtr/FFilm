090becd6d55cceb59bcf5e94f271d961e67d0828
Cảnh Katurtr <bestkatavn8@gmail.com>
2024-07-18 02:45:40 +0700

    Update LoginPage.jsx

--- src/pages/SigninPage/LoginPage.jsx
+++ src/pages/SigninPage/LoginPage.jsx
@@ -1,9 +1,40 @@
 import 'preline/preline';
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import ffilm from '../../assets/image/ffilm.svg'
 import background from '../../assets/image/background.jpg'
+import { useState} from 'react'
+import axios from 'axios'
 
 const LoginPage = () => {
+    const [user, setUser] = useState({
+        username:"",
+        password:""
+    });
+
+    const navigator = useNavigate();
+
+    const [responseMessage, setResponseMessage] = useState('');
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        await axios.post("http://localhost:8000/api/user/login", user)
+        .then((response)=>{
+            alert(response.data.message);
+            navigator('/home');
+            sessionStorage.setItem("token", response.data.refreshToken)
+        })
+        .catch((err)=>{
+            setResponseMessage(err.response?.data?.message || 'An error occurred');
+        })        
+    }
+
+    const changeHandler = (e) => {
+        const { name, value } = e.target;
+        setUser(user => ({
+            ...user,
+            [name]: value
+        }));
+    };
     return (
         <div className='relative flex items-center justify-center min-h-screen min-w-screen bg-cover bg-center' style={{ backgroundImage: `url(${background})` }}>
             <div className="w-full max-w-md mx-auto bg-white/30 backdrop-blur-lg border border-white/30 rounded-xl shadow-sm">
@@ -27,16 +58,15 @@ const LoginPage = () => {
                     <form>
                         <div className="gap-y-4 flex flex-col w-full h-full justify-between">
                         <div>
-                            <label for="email" className="block text-sm mb-2">Email address</label>
+                            <label for="username" className="block text-sm mb-2">Username</label>
                             <div className="relative">
-                            <input type="email" id="email" name="email" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:ring-red-500 disabled:opacity-50 disabled:pointer-events-none" required aria-describedby="email-error"/>
+                            <input onChange={changeHandler} type="text" id="username" name="username" value={user.username} className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:ring-red-500 disabled:opacity-50 disabled:pointer-events-none" required aria-describedby="email-error"/>
                             <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
                                 <svg className="size-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
                                 </svg>
                             </div>
                             </div>
-                            <p className="hidden text-xs text-red-600 mt-2" id="email-error">Please include a valid email address so we can get back to you</p>
                         </div>
                         <div>
                             <div className="flex justify-between items-center">
@@ -44,14 +74,13 @@ const LoginPage = () => {
                             <Link to="/resetPassword" className="text-sm text-red-600 decoration-2 hover:underline font-medium">Forgot password?</Link>
                             </div>
                             <div className="relative">
-                            <input type="password" id="password" name="password" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:ring-red-500 disabled:opacity-50 disabled:pointer-events-none" required aria-describedby="password-error"/>
+                            <input onChange={changeHandler} type="password" id="password" name="password" value={user.password} className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:ring-red-500 disabled:opacity-50 disabled:pointer-events-none" required aria-describedby="password-error"/>
                             <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
                                 <svg className="size-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
                                 </svg>
                             </div>
                             </div>
-                            <p className="hidden text-xs text-red-600 mt-2" id="password-error">8+ characters required</p>
                         </div>
                         <div className="flex items-center">
                             <div className="flex">
@@ -61,8 +90,8 @@ const LoginPage = () => {
                             <label for="remember-me" className="text-sm">Remember me</label>
                             </div>
                         </div>
-
-                        <button type="submit" className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-red-600 text-white hover:bg-red-700 disabled:opacity-50 disabled:pointer-events-none">Sign in</button>
+                        {responseMessage && <p className="text-sm text-center mt-2 text-red-600">{responseMessage}</p>}
+                        <button type="button" onClick={submitHandler} className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-red-600 text-white hover:bg-red-700 disabled:opacity-50 disabled:pointer-events-none">Sign in</button>
                         </div>
                     </form>
                     </div>
