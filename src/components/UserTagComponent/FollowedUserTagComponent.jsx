6774ccaf3cf83ef654fef431380af6583372bccd
Cảnh Katurtr <bestkatavn8@gmail.com>
2024-07-19 20:00:26 +0700

    fix following

--- src/components/UserTagComponent/FollowedUserTagComponent.jsx
+++ src/components/UserTagComponent/FollowedUserTagComponent.jsx
@@ -5,8 +5,13 @@ import Intercept from '../../Tools/refrech'
 
 const FollowedUserTagComponent = (props) => {
   const {user, dispatch} = useContext(AuthContext)
-  const axiosJWT = axios.create()
+  
+  props.onChange(0);
+  
+  const axiosJWT = axios.create();
   Intercept(axiosJWT);
+
+  
   const unFollow = (async () => {
     await axiosJWT.put(
       `http://localhost:8000/api/user/${props.user.username}/unfollow`,
@@ -19,22 +24,21 @@ const FollowedUserTagComponent = (props) => {
     );
     dispatch({ type: "UNFOLLOW", payload: props.user._id });
   })
-  console.log(props)
   return (
     <div>
       <li className="flex items-center justify-between gap-4 ml-4 mr-4"> 
         <div className="flex gap-4 items-center">
-            <img className="size-[62px] rounded-full" src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80" alt="userAavatar"/>
+            <img className="size-[62px] rounded-full" src={props.user.propfilePicture} alt="userAavatar"/>
             <div>
             <div className="text-lg font-bold">{props.user.username}</div>
             <div className="text-xs font-normal">@{props.user.username}</div>
             </div>
         </div>              
-        <span
+        <button
         onClick={unFollow}
         class="inline-flex items-center justify-center size-[38px] text-sm font-semibold rounded-full leading-none border border-red-200 bg-white text-red-800 shadow-xl">
         UnFollow
-        </span>
+        </button>
      </li>   
     </div>
   )
